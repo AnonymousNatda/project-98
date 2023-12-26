d67c70b03bb82c8fddb79cf0af02a44981114b90
AnonymousNatda <150141969+AnonymousNatda@users.noreply.github.com>
2023-12-26 18:58:04 +0530

    Add files via upload

---
+++ project98.py
@@ -0,0 +1,12 @@
+ll = int(input("Enter the lower limit: "))
+ul = int(input("Enter the upper limit: "))
+divisor = int(input("Enter the divisor: "))
+
+for i in range(ll , ul+1):
+    if i % divisor == 0:
+        print(i)
+
+
+
+
+
